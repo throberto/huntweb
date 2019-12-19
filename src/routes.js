36dffa0691af450071e1fcf8322d358a46279359
Thiago Roberto <thiago.nemo4code@gmail.com>
2019-12-19 14:50:34 -0300

    detalhamento de produto

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Main from "./pages/Main";
+import Product from "./pages/Product";
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Main} />
-      <Route path="/products" component={Main} />
+      <Route exact path="/" component={Main} />
+      <Route path="/products/:id" component={Product} />
     </Switch>
   </BrowserRouter>
 );
